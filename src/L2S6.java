76ec7378c367653aae220b65a359cf65d5562afe
alexus <alexus@gmail.com>
2016-01-07 17:37:31 -0500

    +S2S6 - Using Variables

---
+++ src/L2S6.java
@@ -0,0 +1,30 @@
+// S2S6 - Using Variables
+public class L2S6 {
+
+	public static void main(String[] args) {
+		// java primitive types of variables
+		
+		int myNumber = 88;
+		short myShort = 847;
+		long myLong = 9797;
+		
+		double myDouble = 7.3243;
+//		float = myFloat = 324.3f;
+		
+		char myChar = 'y';
+		boolean myBoolean = true;
+		
+		byte myByte = 27;
+
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+//		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+
+	}
+
+}
