d8209f218b00cfea4c21a7cb937b020162afbcd2
alexus <alexus@gmail.com>
2016-01-07 17:39:01 -0500

    +S2S7 - Working With Text

---
+++ src/L2S7.java
@@ -0,0 +1,28 @@
+// S2S7 - Working With Text
+public class L2S7 {
+
+	public static void main(String[] args) {
+
+		int myInt = 7;
+		
+		String text = "Hello";
+		
+		String blank = " ";
+		
+		String name = "Bob";
+		
+		String greeting = text + blank + name;
+		
+		System.out.println(greeting);
+		
+		System.out.println("Hello" + " " + "Bob");
+		
+		System.out.println("My integer is: " + myInt);
+		
+		double myDouble = 7.8;
+		
+		System.out.println("My number is: " + myDouble + ".");
+
+	}
+
+}
