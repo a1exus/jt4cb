b00f4684134492f6520a90d908af29002e604076
alexus <alexus@gmail.com>
2016-01-10 21:14:22 -0500

    L2S8 - While loops

---
+++ src/L2S8.java
@@ -0,0 +1,15 @@
+//L2S8 - While loops
+public class L2S8 {
+
+	public static void main(String[] args) {
+
+		int value = 0;
+		
+		while(value < 10){
+			System.out.println("Hello " + value);
+			
+			value = value + 1;
+		}
+	}
+
+}
