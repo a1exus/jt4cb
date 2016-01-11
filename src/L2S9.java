455eb4a15f1d66f7ec7af1c581a648efbe57d24d
alexus <alexus@gmail.com>
2016-01-10 21:26:07 -0500

    L2S9 - For Loops

---
+++ src/L2S9.java
@@ -0,0 +1,11 @@
+//L2S9 - For Loops
+public class L2S9 {
+
+	public static void main(String[] args) {
+		
+		for(int i=0; i < 5; i++) {
+			System.out.printf("The value of i is: %d\n", i);
+		}
+	}
+
+}
